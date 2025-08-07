c032d3514c6f57ed653b9834a0a890d0b8f48dba
Agustin Botella <agustinbotella90@gmail.com>
2025-08-07 14:29:44 -0300

    Refactor Firebird connection options in test-connection.js for improved clarity and update user credentials

--- test-connection.js
+++ test-connection.js
@@ -1,10 +1,10 @@
 const Firebird = require('node-firebird-dev');
 
 const options = {
-  host: '', // Leave this empty for remote-style path
-  database: '192.168.1.30:/var/lib/firebird/3.0/data/DBSIF.FDB',
-  user: 'LECTURA',
-  password: 'LECTURA'
+  host: '192.168.1.30',
+  database: '/var/lib/firebird/3.0/data/DBSIF.FDB',
+  user: 'SYSDBA',
+  password: 'masterkey' // test with this user!
 };
 
 Firebird.attach(options, (err, db) => {
@@ -14,14 +14,5 @@ Firebird.attach(options, (err, db) => {
   }
 
   console.log('✅ Connected to Firebird');
-
-  db.query('SELECT FIRST 1 * FROM articulos', (err, result) => {
-    db.detach();
-
-    if (err) {
-      console.error('❌ Query failed:', err.message);
-    } else {
-      console.log('✅ Sample data:', result);
-    }
-  });
+  db.detach();
 });
