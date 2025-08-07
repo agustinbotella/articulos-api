e586d681b263652f2d76383d3bf41ec3362666a2
Agustin Botella <agustinbotella90@gmail.com>
2025-08-07 14:21:32 -0300

    Update Firebird connection credentials in test-connection.js

--- test-connection.js
+++ test-connection.js
@@ -1,10 +1,9 @@
 const Firebird = require('node-firebird-dev');
 
 const options = {
-  host: '', // Leave this empty for remote-style path
   database: '192.168.1.30:/var/lib/firebird/3.0/data/DBSIF.FDB',
-  user: 'LECTURA',
-  password: 'LECTURA'
+  user: 'SYSDBA',
+  password: 'masterkey'
 };
 
 Firebird.attach(options, (err, db) => {
@@ -14,14 +13,5 @@ Firebird.attach(options, (err, db) => {
   }
 
   console.log('✅ Connected to Firebird');
-
-  db.query('SELECT FIRST 1 * FROM articulos', (err, result) => {
-    db.detach();
-
-    if (err) {
-      console.error('❌ Query failed:', err.message);
-    } else {
-      console.log('✅ Sample data:', result);
-    }
-  });
+  db.detach();
 });
